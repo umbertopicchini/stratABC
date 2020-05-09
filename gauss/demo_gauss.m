5f9d0686639f160cad447af650b36037001e7db6
Umberto Picchini <1611907+umbertopicchini@users.noreply.github.com>
2020-05-09 09:02:18 +0200

    just some code cleanup. No significant change

--- gauss/demo_gauss.m
+++ gauss/demo_gauss.m
@@ -11,13 +11,12 @@ hyper_sigma = 0.2;  % the prior standard deviation
 
 sobs = mean(data)
 nsummary = length(sobs);
-collectedsummaries = [];
-acceptedsummaries = [];
 
-numsimABC = 500;
+numsimABC = 500; % the number of model simulations at each proposed parameter
 mu_start = 0;
-R_mcmc = 10000;
+R_mcmc = 10000;  % the number of MCMC iterations
 burnin = 1000;
+delta = 3e-5;  % the ABC threshold
 
 mu_old = mu_start;
 MCMC = zeros(R_mcmc,1);
@@ -25,11 +24,7 @@ MCMC(1) = mu_old;
 
 simdata = mu_old + randn(nobs,numsimABC);
 simsumm = mean(simdata,1);
-distance = sqrt((simsumm-sobs).^2);
-delta = 3e-5;  % was 1e-4
-collectedsummaries = [collectedsummaries;simsumm'] ;
-% delta = prctile(distance,quantile);
-%loglike_old =  log(sum(distance < delta));
+distance = sqrt((simsumm-sobs).^2);  
 loglike_old = -nsummary*log(delta) +logsumexp(-distance.^2/(2*delta^2));
 logprior_old = -log(hyper_sigma) - 0.5*(mu_old-hyper_mu)^2 / hyper_sigma^2;
 
@@ -40,13 +35,7 @@ for mcmc_iter = 2:R_mcmc
   mu = mu_old + 0.1*randn;
   simdata = mu + randn(nobs,numsimABC);  % simulated data
   simsumm = mean(simdata,1);             % summary of simulated data
-  if mcmc_iter < burnin
-     collectedsummaries = [collectedsummaries;simsumm'] ;
-  end
   distance = sqrt((simsumm-sobs).^2);
-%  index_inclusion1 = distance < delta;
-%  n1 = sum(index_inclusion1);
-  %loglike =  log(sum(distance < delta));
   loglike = -nsummary*log(delta) +logsumexp(-distance.^2/(2*delta^2));
   logprior = -log(hyper_sigma) - 0.5*(mu-hyper_mu)^2 / hyper_sigma^2;
   if log(rand) < loglike-loglike_old + logprior - logprior_old
@@ -55,11 +44,6 @@ for mcmc_iter = 2:R_mcmc
       mu_old = mu;
       loglike_old = loglike;
       logprior_old = logprior;
-    %  n1
-      if mcmc_iter > burnin
-         acceptedsummaries = [acceptedsummaries;simsumm'] ;
-      end
-     % delta = min(delta,prctile(distance,quantile));
   else
       MCMC(mcmc_iter) = mu_old;
   end
@@ -71,24 +55,21 @@ figure
 plot(MCMC);
 titlestring = sprintf('numsimABC = %d, delta = %d',numsimABC,delta);
 title(titlestring)
-%legend('ABC')
 figure
 ksdensity(MCMC(burnin:end));
 hold on
 % plot EXACT POSTERIOR using a conjugate Gaussian prior for mu
 posterior_mean = 1/(1/hyper_sigma^2 + nobs/sigma^2) * (hyper_mu/hyper_sigma^2 + sum(data)/sigma^2);
 posterior_std = (1/hyper_sigma^2 + nobs/sigma^2)^(-0.5);
-%posterior_draws = posterior_mean + posterior_std * randn(10000,1);
 x = [-0.15:.0001:0.15];
 y = normpdf(x,posterior_mean,posterior_std);
 plot(x,y)
-%ksdensity(posterior_draws)
 legend('ABC','Exact Bayes')
 
 fprintf('\nABC inference for MU')
 [mean(MCMC(burnin:end)), prctile(MCMC(burnin:end),[2.5 97.5])]
 fprintf('\nExact inference for MU')
 [posterior_mean,posterior_mean-1.96*posterior_std,posterior_mean+1.96*posterior_std]
-%prctile(posterior_draws,[2.5 50 97.5])
+
 
 
